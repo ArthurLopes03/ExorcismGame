61728e8b31b127e917a2a2684254a8738500513e
ArthurLopes03 <C22509166@mytudublin.ie>
2024-10-05 14:21:02 +0100

    Randomized clues
    Fixed the bug where clue position is tied to the aspect, also added additional clues

--- Assets/Scripts/Clue Factory.cs
+++ Assets/Scripts/Clue Factory.cs
@@ -7,28 +7,68 @@ public class ClueFactory : MonoBehaviour
 {
     public GameObject cluePrefab;
     public Transform listObject;
+    List<Clue> cluesList;
 
+    public int randomClues;
     public void CreateClues(Demon demon)
     {
-        List<Clue> clues = new List<Clue>();
+        cluesList = new List<Clue>();
 
-        SetClue(demon.kingdom.clues);
+        cluesList.Add(SetClue(demon.kingdom.clues));
+        cluesList.Add(SetClue(demon.type.clues));
+        cluesList.Add(SetClue(demon.temperment.clues));
 
-        SetClue(demon.type.clues);
+        AddRandomClue(demon, randomClues);
 
-        SetClue(demon.temperment.clues);
+        cluesList.Shuffle();
+
+        foreach (Clue clue in cluesList)
+        {
+            TextMeshProUGUI text = Instantiate(cluePrefab, listObject).GetComponentInChildren<TextMeshProUGUI>();
+
+            text.text = clue.description;
+        }
     }
 
-    void SetClue(List<Clue> clues)
+    Clue SetClue(List<Clue> clues)
     {
         Clue clue = null;
 
+        tryagain:
         int r = Random.Range(0, clues.Count);
 
-        clue = clues[r];
+        if (cluesList.Contains(clues[r]))
+        {
+            goto tryagain;
+        }
+        else
+        {
+            clue = clues[r];
+        }
 
-        TextMeshProUGUI text = Instantiate(cluePrefab, listObject).GetComponentInChildren<TextMeshProUGUI>();
+        return clue;
+    }
 
-        text.text = clue.description;
+    void AddRandomClue(Demon demon, int amountToAdd)
+    {
+        List<Clue> cluePool = new List<Clue>();
+        cluePool.AddRange(demon.kingdom.clues);
+        cluePool.AddRange(demon.type.clues);
+        cluePool.AddRange(demon.temperment.clues);
+
+        for(int i = 0; i < amountToAdd; i++)
+        {
+            tryagain:
+            int r = Random.Range(0, cluePool.Count);
+
+            if (cluesList.Contains(cluePool[r]))
+            {
+                goto tryagain;
+            }
+            else
+            {
+                cluesList.Add(cluePool[r]);
+            }
+        }
     }
 }
